ea2cc1b21a306b9fdd8a4e9e0ff0d171f9244391
ryant <ryan.thorp@my.jcu.edu.au>
2016-08-07 13:37:35 +1000

    Fixed Debugging Exercises

---
+++ DebuggingExercises/FixedDebugOne3.java
@@ -0,0 +1,8 @@
+public class FixedDebugOne3 {
+
+    public static void main(String[] args) {
+        System.out.println("Over the river");
+        System.out.println("and through the woods");
+        System.out.println("to Grandmother's house we go.");
+    }
+}
